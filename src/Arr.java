5537689388eb3f7493ff41fa6cc0da7006d28195
Alexandra Bystrova <saobyjob90@gmail.com>
2022-02-02 20:17:35 +0300

    First commit

---
+++ src/Arr.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+public class Arr {
+    public static void main(String[] args) {
+        int [] nums = {5, 24, 3, 66, 38, 16, 27};
+        Arrays.sort(nums);
+        System.out.println(nums [2]);
+    }
+}
